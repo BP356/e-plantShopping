cab0b88da1063b1806b0f5c274a315fae192ee2d
BP356 <barbara.prohaska@gmx.at>
2025-01-21 02:38:56 -0500

    Task 1 complete

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,28 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    const item = state.iems.find(i => i.id === action.payload.id);
-      if (item) {
-        item.quantity += 1;
-      }
-      else {
-        state.items.push({...action.payload, quantity: 1});
-      }
+    name: 'cart',
+    initialState: {
+        items: [], // Initialize items as an empty array
+    },
+    reducers: {
+        addItem: (state, action) => {
+            const item = state.items.find(i => i.name === action.payload.name);
+            if (item) {
+                item.quantity += 1;
+            }
+            else {
+                state.items.push({ ...action.payload, quantity: 1 });
+            }
+        },
+        removeItem: (state, action) => {
+        },
+        updateQuantity: (state, action) => {
 
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
 
-    
+        },
     },
-  },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, Fragment } from 'react';
+import { useDispatch } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
@@ -7,6 +8,7 @@ function ProductList() {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -250,8 +252,13 @@ function ProductList() {
         setShowCart(false);
     };
 
-    const handleAddToCart = (plant) = {
+    const handleAddToCart = (plant) => {
         dispatch(addItem(plant));
+        setAddedToCart({...addedToCart, [plant.name]: true});
+        // setAddedToCart((prevState) => ({
+        //     ...prevState,
+        //     [plant.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+        //   }));
     }
 
     return (

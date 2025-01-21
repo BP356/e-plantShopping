31123733c534ddd063a3667b67a729ea7ebe15b3
BP356 <barbara.prohaska@gmx.at>
2025-01-21 06:08:56 -0500

    style not perfect

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -241,6 +241,11 @@ function ProductList() {
     }, [cart])
 
     const styleObj = {
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        width: '100%',
+        zIndex: '1000',
         backgroundColor: '#4CAF50',
         color: '#fff!important',
         padding: '15px',
@@ -264,7 +269,6 @@ function ProductList() {
         position: 'absolute',
         top: '7px',
         right: '7px',
-        // backgroundColor: '#ff4d4f',
         color: 'white',
         borderRadius: '50%',
         width: '50px',
@@ -312,10 +316,11 @@ function ProductList() {
                             </div>
                         </a>
                     </div>
-
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
+                </div>
+                <div style={styleObjUl}>
                     <div style={{ position: 'relative', display: 'inline-block' }}>
                         <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
                             <h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none">5</rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1>
@@ -325,10 +330,10 @@ function ProductList() {
                 </div>
             </div>
             {!showCart ? (
-                plantsArray.map(category => (
-                    <Fragment>
-                        <h1 className='product-list'>{category.category}</h1>
+                <div className='products'>
+                    {plantsArray.map(category => (
                         <div className="product-grid">
+                            <h1>{category.category}</h1>
                             <div className='product-list'>
                                 {category.plants.map(plant => (
                                     <div className='product-card'>
@@ -341,8 +346,8 @@ function ProductList() {
                                 ))}
                             </div>
                         </div>
-                    </Fragment>
-                ))
+                    ))}
+                </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
             )}
